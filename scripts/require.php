e5c9f06bf7d7d649a56b10d9de161f8345f7305c
asao Kamei <asao@ubuPC.(none)>
2013-02-26 12:10:47 +0900

    requires QueryInterface.

--- scripts/require.php
+++ scripts/require.php
@@ -3,4 +3,5 @@ require( __DIR__ . '/../src/WScore/DbAccess/DbConnect.php' );
 require( __DIR__ . '/../src/WScore/DbAccess/QueryObject.php' );
 require( __DIR__ . '/../src/WScore/DbAccess/SqlBuilder.php' );
 require( __DIR__ . '/../src/WScore/DbAccess/DbAccess.php' );
+require( __DIR__ . '/../src/WScore/DbAccess/QueryInterface.php' );
 require( __DIR__ . '/../src/WScore/DbAccess/Query.php' );
