29bafef34ed9500f795d4f155a881e6d66d824a5
Asao <kamei@workspot.jp>
2013-03-31 12:44:28 +0900

    set up logger using Monolog.

--- scripts/dbaccess.php
+++ scripts/dbaccess.php
@@ -1,6 +1,12 @@
 <?php
 namespace WScore\DbAccess;
 
+use Monolog\Handler\NullHandler;
+use Monolog\Logger;
+
+$log = new Logger( 'DbAccess' );
+$log->pushHandler( new NullHandler() );
 $dba = new DbAccess();
 $dba->sqlBuilder = new SqlBuilder();
+$dba->log = new Profile( $log );
 return $dba->connect( new DbConnect() );
