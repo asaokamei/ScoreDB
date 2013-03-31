da01e4bf8b8e95054be58a742d0e3eae9bc6c8e4
Asao <kamei@workspot.jp>
2013-03-31 12:43:33 +0900

    use compser's autoloader.

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -1,9 +1,17 @@
 <?php
+if( defined( 'VENDOR_DIRECTORY' ) ) { return; }
+elseif( file_exists( __DIR__ . '/../vendor/' ) ) {
+    define( 'VENDOR_DIRECTORY', __DIR__ . '/../vendor/' );
+}
+elseif( file_exists( __DIR__ . '/../../../../vendor/' ) ) {
+    define( 'VENDOR_DIRECTORY', __DIR__ . '/../../../../vendor/' );
+}
+else {
+    die( 'vendor directory not found' );
+}
+require_once( VENDOR_DIRECTORY . 'autoload.php' );
+$loader = new \Composer\Autoload\ClassLoader();
 
-spl_autoload_register(
-    function( $c ) {
-        $path = __DIR__ . '/' . strtr( $c, '\\_', '//' ).'.php';
-        @include_once $path;
-    }
-);
+$loader->add( 'WSTest',   __DIR__ );
+$loader->register();
 
