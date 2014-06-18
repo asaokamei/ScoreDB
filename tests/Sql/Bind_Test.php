04a1342d86fe95878436b468c8cb1a8862d9abcb
Asao <kamei@workspot.jp>
2014-06-18 15:39:09 +0900

    test Bind.

---
+++ tests/Sql/Bind_Test.php
@@ -0,0 +1,41 @@
+<?php
+namespace tests\Sql;
+
+use WScore\DbAccess\Sql\Bind;
+
+require_once( dirname(__DIR__).'/autoloader.php');
+
+class Bind_Test extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Bind
+     */
+    var $b;
+    
+    function setup()
+    {
+        $this->b = new Bind();
+    }
+    
+    function get($head='value') {
+        return $head . mt_rand(1000,9999);
+    }
+    
+    function test0()
+    {
+        $this->assertEquals( 'WScore\DbAccess\Sql\Bind', get_class( $this->b ) );
+    }
+
+    /**
+     * @test
+     */
+    function prepare_replaces_value_with_holder_and_saves_it()
+    {
+        $value = $this->get();
+        $holder = $this->b->prepare($value);
+        $bind  = $this->b->getBinding();
+        
+        $this->assertTrue( isset( $bind[$holder]) );
+        $this->assertEquals( $value, $bind[$holder] );
+    }
+}
