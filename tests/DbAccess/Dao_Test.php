67245efa29fdaece95e18ffb3db408be2348a476
Asao <kamei@workspot.jp>
2014-06-22 20:38:40 +0900

    testing Dao and DbAccess.

--- tests/DbAccess/Dao_Test.php
+++ tests/DbAccess/Dao_Test.php
@@ -25,46 +25,25 @@ class Dao_Test extends \PHPUnit_Framework_TestCase
         $this->user = new User();
     }
     
+    function makeUserData( $idx=1 )
+    {
+        $data = [
+            'name' => 'test-' . $idx ,
+            'age'  => 30 + $idx,
+            'bday' => (new \DateTime('1989-01-01'))->add(new \DateInterval('P1D'))->format('Y-m-d'),
+            'no_null' => 'not null test: ' . mt_rand(1000,9999),
+        ];
+        return $data;
+    }
+    
     function test0()
     {
         $this->assertEquals( 'tests\DbAccess\Dao\User', get_class( $this->user ) );
     }
     
-    function test_extendedPdo()
-    {
-        $sql = 'INSERT INTO `test_WScore` ( `name`, `age`, `bday`, `no_null` ) VALUES ( :db_prep_1, :db_prep_2, :db_prep_3, :db_prep_4 )';
-        $bind = [
-            'db_prep_1' => 'test',
-            'db_prep_2' => '30',
-            'db_prep_3' => '1989-01-01',
-            'db_prep_4' => 'not null test',
-        ];
-        $pdo = Dba::db();
-        $pdo->perform( $sql, $bind );
-    }
-
-    function test_pdo()
-    {
-        $sql = 'INSERT INTO `test_WScore` ( `name`, `age`, `bday`, `no_null` ) VALUES ( :db_prep_1, :db_prep_2, :db_prep_3, :db_prep_4 )';
-        $bind = [
-            ':db_prep_1' => 'test',
-            ':db_prep_2' => '30',
-            'db_prep_3' => '1989-01-01',
-            'db_prep_4' => 'not null test',
-        ];
-        $pdo = Dba::db();
-        $pdo = $pdo->getPdo();
-        $stm = $pdo->prepare( $sql );
-        $stm->execute( $bind );
-    }
-
     function test_insert()
     {
-        $id = $this->user->insert([
-            'name' => 'test',
-            'age'  => '30',
-            'bday' => '1989-01-01',
-            'no_null' => 'not null test',
-        ]);
+        $user = $this->makeUserData();
+        $id = $this->user->insert( $user );
     }
 }
