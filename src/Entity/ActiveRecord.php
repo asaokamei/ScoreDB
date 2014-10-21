77aab7f58200640f80eb3b78f22402effee9c88d
Asao <kamei@workspot.jp>
2014-10-21 09:56:18 +0900

    should reflect the original data to the just saved data, for both insert and update.

--- src/Entity/ActiveRecord.php
+++ src/Entity/ActiveRecord.php
@@ -43,8 +43,8 @@ class ActiveRecord extends EntityObject
             $id = $dao::inject( $this->_data );
             $this->__set( $this->_keyName, $id );
             $this->_isFetched = true;
-            $this->_original_data = $this->_data;
         }
+        $this->_original_data = $this->_data;
         return $this;
     }
 
