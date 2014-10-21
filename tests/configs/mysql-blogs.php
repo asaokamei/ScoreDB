03358707315044cf50525e165d498d246e24a45f
Asao <kamei@workspot.jp>
2014-10-21 09:43:23 +0900

    changed tag_id to varchar(64); used heredocs.

--- tests/configs/mysql-blogs.php
+++ tests/configs/mysql-blogs.php
@@ -1,47 +1,50 @@
 <?php
-return '
-        DROP TABLE IF EXISTS dao_user;
-        DROP TABLE IF EXISTS dao_blog;
-        DROP TABLE IF EXISTS dao_tag;
-        DROP TABLE IF EXISTS dao_blog_tag;
+return <<<'BLOG_SQL'
 
-        CREATE TABLE dao_user (
-            user_id int NOT NULL AUTO_INCREMENT,
-            name VARCHAR(30),
-            gender int,
-            status int,
-            age  int,
-            bday date,
-            no_null text NOT NULL,
-            open_date   date,
-            created_at  datetime,
-            updated_at  datetime,
-            PRIMARY KEY (user_id)
-        );
+DROP TABLE IF EXISTS dao_user;
+DROP TABLE IF EXISTS dao_blog;
+DROP TABLE IF EXISTS dao_tag;
+DROP TABLE IF EXISTS dao_blog_tag;
 
-        CREATE TABLE dao_blog (
-          blog_id int NOT NULL AUTO_INCREMENT,
-          user_id INT,
-          status int,
-          title TEXT,
-          content TEXT,
-          created_at DATETIME,
-          updated_at DATETIME,
-          PRIMARY KEY ( blog_id )
-        );
-        
-        CREATE TABLE dao_tag (
-          tag_id INT NOT NULL AUTO_INCREMENT,
-          tag TEXT,
-          created_at DATETIME,
-          updated_at DATETIME,
-          PRIMARY KEY (tag_id)
-        );
-        
-        CREATE TABLE dao_blog_tag (
-          tag_id int NOT NULL ,
-          blog_id INT NOT NULL,
-          created_at DATETIME,
-          PRIMARY KEY (tag_id, blog_id)
-        )
-        ';
+CREATE TABLE dao_user (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    gender INT,
+    status INT,
+    age  INT,
+    bday DATE,
+    no_null TEXT NOT NULL,
+    open_date   DATE,
+    created_at  DATETIME,
+    updated_at  DATETIME,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE dao_blog (
+  blog_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT,
+  status INT,
+  title TEXT,
+  content TEXT,
+  created_at DATETIME,
+  updated_at DATETIME,
+  PRIMARY KEY ( blog_id )
+);
+
+CREATE TABLE dao_tag (
+  tag_id VARCHAR(64),
+  tag TEXT,
+  created_at DATETIME,
+  updated_at DATETIME,
+  PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE dao_blog_tag (
+  tag_id VARCHAR(64),
+  blog_id INT NOT NULL,
+  created_at DATETIME,
+  PRIMARY KEY (tag_id, blog_id)
+)
+
+BLOG_SQL
+    ;
--- tests/configs/pgsql-blogs.php
+++ tests/configs/pgsql-blogs.php
@@ -1,47 +1,50 @@
 <?php
-return '
-        DROP TABLE IF EXISTS dao_user;
-        DROP TABLE IF EXISTS dao_blog;
-        DROP TABLE IF EXISTS dao_tag;
-        DROP TABLE IF EXISTS dao_blog_tag;
+return <<<'BLOG_SQL'
 
-        CREATE TABLE dao_user (
-            user_id SERIAL,
-            name VARCHAR(30),
-            gender int,
-            status int,
-            age  int,
-            bday date,
-            no_null text NOT NULL,
-            open_date   date,
-            created_at  timestamp,
-            updated_at  timestamp,
-            PRIMARY KEY (user_id)
-        );
+DROP TABLE IF EXISTS dao_user;
+DROP TABLE IF EXISTS dao_blog;
+DROP TABLE IF EXISTS dao_tag;
+DROP TABLE IF EXISTS dao_blog_tag;
 
-        CREATE TABLE dao_blog (
-          blog_id SERIAL,
-          user_id INT,
-          status INT,
-          title TEXT,
-          content TEXT,
-          created_at TIMESTAMP,
-          updated_at TIMESTAMP,
-          PRIMARY KEY ( blog_id )
-        );
-        
-        CREATE TABLE dao_tag (
-          tag_id SERIAL,
-          tag TEXT,
-          created_at TIMESTAMP,
-          updated_at TIMESTAMP,
-          PRIMARY KEY (tag_id)
-        );
-        
-        CREATE TABLE dao_blog_tag (
-          tag_id INT NOT NULL ,
-          blog_id INT NOT NULL,
-          created_at TIMESTAMP,
-          PRIMARY KEY (tag_id, blog_id)
-        )
-        ';
+CREATE TABLE dao_user (
+    user_id SERIAL,
+    name VARCHAR(30),
+    gender INT,
+    status INT,
+    age  INT,
+    bday DATE,
+    no_null TEXT NOT NULL,
+    open_date   DATE,
+    created_at  TIMESTAMP,
+    updated_at  TIMESTAMP,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE dao_blog (
+  blog_id SERIAL,
+  user_id INT,
+  status INT,
+  title TEXT,
+  content TEXT,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  PRIMARY KEY ( blog_id )
+);
+
+CREATE TABLE dao_tag (
+  tag_id VARCHAR(64),
+  tag TEXT,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE dao_blog_tag (
+  tag_id VARCHAR(64),
+  blog_id INT NOT NULL,
+  created_at TIMESTAMP,
+  PRIMARY KEY (tag_id, blog_id)
+)
+
+BLOG_SQL
+;
